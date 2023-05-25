4e5607135d5b06bceae95315c47b30c0e9b5ab76
scheng0718 <scheng0718@gmail.com>
2023-05-25 10:52:11 -0700

    feat: add mongoose

--- app.js
+++ app.js
@@ -5,7 +5,22 @@ const app = express()
 const mongoose = require('mongoose')
 const port = 3000
 
-mongoose.connect(process.env.MONGODB_URI)
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
+
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true,  useUnifiedTopology: true})
+// 取得連線狀態
+const db = mongoose.connection
+// 連線異常
+db.on('error', () => {
+  console.log('mongodb error!')
+})
+// 連線成功
+db.once('open', () => {
+  console.log('mongodb connected!')
+})
+
 app.get('/', (req, res) => {
   res.send('This is application of todo-list')
 })
