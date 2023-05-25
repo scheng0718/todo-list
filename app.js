987dacfd676c6a88fb80274865337f6ebda12310
scheng0718 <scheng0718@gmail.com>
2023-05-25 10:04:12 -0700

    install and set up mongoose

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
-// Load express module
+// Load express module and use express
 const express = require('express')
 const app = express()
+// Load mongoose
+const mongoose = require('mongoose')
 const port = 3000
 
+mongoose.connect(process.env.MONGODB_URI)
 app.get('/', (req, res) => {
   res.send('This is application of todo-list')
 })
