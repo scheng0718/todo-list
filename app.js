e5a555217c024075c840c1404a632873340a55c7
scheng0718 <scheng0718@gmail.com>
2023-05-30 14:40:59 -0700

    feat: add delete todo function

--- app.js
+++ app.js
@@ -82,6 +82,14 @@ app.post('/todos/:id/edit', (req, res) => {
     .catch(error => console.log(error))
 })
 
+app.post('/todos/:id/delete', (req, res) => {
+  const id = req.params.id
+  return Todo.findById(id)
+    .then(todo => todo.remove())
+    .then(() => res.redirect('/'))
+    .catch(error => console.log(error))
+})
+
 app.listen(port, () => {
   console.log(`The server is running at http://localhost:${port}`)
 })
