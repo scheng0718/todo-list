2a1051188dde3c69c3ab7087351cdf89b670482b
scheng0718 <scheng0718@gmail.com>
2023-06-06 19:15:05 -0700

    refactor: modify mongoos config

--- app.js
+++ app.js
@@ -1,7 +1,5 @@
 // Load express module and use express
 const express = require('express')
-// Load mongoose
-const mongoose = require('mongoose')
 // Load express handlebars
 const exphbs = require('express-handlebars')
 // Load body-parser
@@ -10,29 +8,18 @@ const bodyParser = require('body-parser')
 const methodOverride = require('method-override')
 
 const routes = require('./routes')
+require('./config/mongoose')
+
 const app = express()
 const port = 3000
-
-if (process.env.NODE_ENV !== 'production') {
-  require('dotenv').config()
-}
-mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true,  useUnifiedTopology: true})
-// 取得連線狀態
-const db = mongoose.connection
-// 連線異常
-db.on('error', () => {
-  console.log('mongodb error!')
-})
-// 連線成功
-db.once('open', () => {
-  console.log('mongodb connected!')
-})
 
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs'}))
 app.set('view engine', 'hbs')
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
+
 app.use(routes)
+
 app.listen(port, () => {
   console.log(`The server is running at http://localhost:${port}`)
 })
--- models/seeds/todoSeeder.js
+++ models/seeds/todoSeeder.js
@@ -1,18 +1,7 @@
-// load mongoose
-const mongoose = require('mongoose')
 // ../ 是引用父目錄
 const Todo = require('../todo')
+const db = require('../../config/mongoose')
 
-if (process.env.NODE_ENV !== 'production') {
-  require('dotenv').config()
-}
-mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
-// 取得連線物件
-const db = mongoose.connection
-// 設定 error 事件監聽器
-db.on('error', () => {
-  console.log('mongo error')
-})
 // 設定 open 事件監聽器
 db.once('open', () => {
   console.log('mongodb connected')
