a01879e7a6a68b4c6cbf88377f945bf4d3ec6e56
scheng0718 <scheng0718@gmail.com>
2023-06-12 12:46:28 -0700

    chore: heroku init

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ const routes = require('./routes')
 require('./config/mongoose')
 
 const app = express()
-const port = 3000
+const PORT = process.env.PORT || 3000
 
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs'}))
 app.set('view engine', 'hbs')
@@ -19,6 +19,6 @@ app.use(bodyParser.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
 app.use(routes)
 
-app.listen(port, () => {
-  console.log(`The server is running at http://localhost:${port}`)
+app.listen(PORT, () => {
+  console.log(`The server is running at http://localhost:${PORT}`)
 })
