914275ce98a861001a65f112c896daf844df5572
scheng0718 <scheng0718@gmail.com>
2023-07-10 18:39:04 -0700

    modify todoSeeder

--- models/seeds/todoSeeder.js
+++ models/seeds/todoSeeder.js
@@ -1,13 +1,39 @@
+const bcrypt = require('bcryptjs')
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
 // ../ 是引用父目錄
 const Todo = require('../todo')
+const User = require('../user')
 const db = require('../../config/mongoose')
-
+const SEED_USER = {
+  name: 'root',
+  email: 'root@example.com',
+  password: '12345678'
+}
 // 設定 open 事件監聽器
 db.once('open', () => {
-  console.log('mongodb connected')
-  // 迴圈建立十筆種子資料
-  for (let i = 0; i < 10; i++) {
-    Todo.create({name: `name-${i}`})
-  }
-  console.log('todo created')
+  bcrypt
+    .genSalt(10)
+    .then(salt => bcrypt.hash(SEED_USER.password, salt))
+    .then(hash => User.create({
+      name: SEED_USER.name,
+      email: SEED_USER.email,
+      password: hash
+    }))
+    .then(user => {
+      const userId = user._id
+      for (let i = 0; i < 10; i++) {
+        return Promise.all(Array.from(
+          { length: 10 },
+          (_, i) => Todo.create({ 
+            name: `name-${i}`,userId
+          })
+        ))
+      }
+    })
+    .then(() => {
+      console.log('done.')
+      process.exit()
+    })
 })
