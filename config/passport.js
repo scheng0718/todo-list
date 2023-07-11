e01e2a0ebf22c5f5c64fef3c6d52a87b8bbbfc5a
scheng0718 <scheng0718@gmail.com>
2023-07-10 17:08:34 -0700

    feat: add facebook login and store sensitive data in dotenv

--- config/passport.js
+++ config/passport.js
@@ -1,5 +1,6 @@
 const passport = require('passport')
 const LocalStrategy = require('passport-local').Strategy
+const FacebookStrategy = require('passport-facebook').Strategy
 const bcrypt = require('bcryptjs')
 const User = require('../models/user')
 module.exports = (app) => {
@@ -22,6 +23,35 @@ module.exports = (app) => {
       })
       .catch(error => done(error, false))
   }))
+  passport.use(new FacebookStrategy({
+    clientID: process.env.FACEBOOK_ID,
+    clientSecret: process.env.FACEBOOK_SECRET,
+    callbackURL: process.env.FACEBOOK_CALLBACK,
+    profileFields: ['email', 'displayName']
+  }, (accessToken, refreshToken, profile, done) => {
+    // console.log(profile)
+    const { name, email } = profile._json
+    User.findOne({ email })
+      .then(user => {
+        if (user) {
+          return done(null, user)
+        }
+        // 就算是用 facebook 第三方登入，還是需要為使用者製作密碼
+        const randomPassword = Math.random().toString(36).slice(-8)
+        // console.log(randomPassword)
+        bcrypt
+          .genSalt(10)
+          .then(salt => bcrypt.hash(randomPassword, salt))
+          .then(hash => User.create({
+            name,
+            email,
+            password: hash
+          }))
+          .then(user => done(null, user))
+          .catch(error => console.log(error))
+      })
+  }))
+
   // Set up serialization and deserialization 
   passport.serializeUser((user, done) => {
     done(null, user.id)
